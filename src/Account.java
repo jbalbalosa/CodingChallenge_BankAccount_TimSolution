bc8e9e49785cf7b9c765fb5e95a34b227be85eef
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-12-07 14:35:56 +0900

    update

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,85 @@
-package PACKAGE_NAME;public class Account {
+public class Account {
+
+    private String accountNumber;
+    private double accountBalance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+    //Initialize constructor
+    public Account(){
+        System.out.println("Empty constructor called");
+    }
+
+    public Account(String number, double balance, String customerName, String customerEmailAddress,
+                   String customerPhoneNumber){
+        System.out.println("Account constructor with parameters called");
+        this.accountNumber = number;
+        this.accountBalance = balance;
+        this.customerName = customerName;
+        this.customerEmailAddress = customerEmailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public Account(String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        this("99999", 100.00, customerName, customerEmailAddress, customerPhoneNumber );
+        this.customerName = customerName;
+        this.customerEmailAddress = customerEmailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public void deposit(double amount){
+        this.accountBalance += amount;
+        System.out.println("Deposit of " + amount + " made. New balance is " + this.accountBalance);
+    }
+
+    public void withdrawal(double amount){
+        if(accountBalance - amount < 0){
+            System.out.println("only " +  this.accountBalance + " available. Withdrawal not processed");
+        } else {
+            this.accountBalance -= amount;
+            System.out.println("Withdrawal of " + amount + " processed. Remaining Balance" + this.accountBalance);
+        }
+
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Account myAccount = new Account("123-456-789", 0.00,
+                "Jeff", "@gmail", "778-2572");
+
+
+        //Empty constructor
+        Account secondAccount = new Account();
+
+
+/*      No need for this because it was already defined above with constructor
+        myAccount.setAccountNumber("123-456");
+        myAccount.setAccountBalance(0.0);
+        myAccount.setCustomerName("Jeff");
+        myAccount.setCustomerEmailAddress("me@yahoo.com");
+        myAccount.setCustomerPhoneNumber("+680-778-2572");*/
+
+
+        myAccount.withdrawal(100.0);
+
+        myAccount.deposit(50.0);
+        myAccount.withdrawal(100.0);
+
+        myAccount.deposit(51.0);
+        myAccount.withdrawal(100.0);
+
+        Account jeffAccount = new Account("Jeff", "jeffbalbalosa@gmail", "123456");
+        System.out.println("Account Number: " + jeffAccount.getAccountNumber() + " Balance: " + jeffAccount.getAccountBalance() +
+                " Account Number: " + jeffAccount.getCustomerName());
     }
 }
